78f6e9feca8bbbcb6636f210309452945734252e
Rahul Zhade <rzhade3@users.noreply.github.com>
2021-05-21 16:28:19 -0700

    Update tf template to zip up payload

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
+data "archive_file" "payload" {
+  type        = "zip"
+  source_dir = "src"
+  output_path = "${path.module}/.terraform/modules/lambda-function-credential-exfiltration/payload.zip"
+}
+
 resource "aws_lambda_function" "this" {
-  filename      = ".terraform/modules/lambda-function-credential-exfiltration/payload.zip"
+  filename      = data.archive_file.payload.output_path
   function_name = "test"
   role          = var.role_arn
   handler       = "index.handler"
 
-  source_code_hash = filebase64sha256(".terraform/modules/lambda-function-credential-exfiltration/payload.zip")
+  source_code_hash = filebase64sha256(data.archive_file.payload.output_path)
 
   runtime = "nodejs12.x"
 }
